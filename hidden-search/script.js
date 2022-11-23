85092e6b064639c3d1721757e890b03058b94fab
y2610-11 <andrew.vi.2610@gmail.com>
2022-11-23 10:49:42 +0100

    complete the 4 proyect

---
+++ hidden-search/script.js
@@ -0,0 +1,12 @@
+let input = document.querySelector('.input')
+let btn = document.querySelector('.btn')
+
+btn.addEventListener('click',function(){
+input.classList.toggle('input-animation')
+btn.classList.toggle('btn_animation1')
+
+
+
+
+})
+
