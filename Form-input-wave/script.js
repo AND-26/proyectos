c747cd4648bd1fa6a718b6c83431bc93cb6174ef
y2610-11 <andrew.vi.2610@gmail.com>
2022-11-30 09:20:24 +0100

    form-input-wave

---
+++ Form-input-wave/script.js
@@ -0,0 +1,26 @@
+const labels = document.querySelectorAll('.form-control label')  
+ labels.forEach(label => {  
+   
+label.addEventListener('click',()=>{
+
+
+for (let i = 0; i < label.length; i++) {
+    
+    console.log(label);
+    
+}
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+ })  
