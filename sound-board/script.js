02fe7baf39248e2e8cd198f32336e04602fda16e
y2610-11 <andrew.vi.2610@gmail.com>
2022-12-05 14:04:15 +0100

    finish the proyect sound-bar

--- sound-board/script.js
+++ sound-board/script.js
@@ -6,10 +6,9 @@ const song = new Audio(
 btn.addEventListener("click", () => {
   song.play();
   song1.pause();
-  song3.pause()
+  song3.pause();
   song4.pause();
   song2.pause();
-
 });
 ////
 
@@ -21,10 +20,9 @@ const song1 = new Audio(
 btn1.addEventListener("click", () => {
   song1.play();
   song.pause();
-  song3.pause()
+  song3.pause();
   song4.pause();
   song2.pause();
-
 });
 ////
 
@@ -37,47 +35,36 @@ btn2.addEventListener("click", () => {
   song2.play();
   song1.pause();
   song.pause();
-  song3.pause()
+  song3.pause();
   song4.pause();
-
 });
 
 ////
 
-let btn3= document.getElementById("sonido3");
+let btn3 = document.getElementById("sonido3");
 const song3 = new Audio(
   "https://sound-effects-media.bbcrewind.co.uk/mp3/NHU1384-12.mp3?download&rename=BBC__NHU1384-12"
 );
 
 btn3.addEventListener("click", () => {
-song.pause();
+  song.pause();
   song2.pause();
   song1.pause();
   song3.play();
-  song4.pause()
-
+  song4.pause();
 });
-
 
 ////
 
-let btn4= document.getElementById("sonido4");
+let btn4 = document.getElementById("sonido4");
 const song4 = new Audio(
   "https://sound-effects-media.bbcrewind.co.uk/mp3/07011194.mp3?download&rename=BBC_Cats---A-k_07011194"
 );
 
 btn4.addEventListener("click", () => {
-song.pause();
+  song.pause();
   song2.pause();
   song1.pause();
   song3.pause();
   song4.play();
-
 });
-
-
-
-
-
-
-
