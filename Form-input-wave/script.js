c9a076e5450cb7bf62b01e75462cf30df683862c
y2610-11 <andrew.vi.2610@gmail.com>
2022-12-05 09:38:47 +0100

    Finished the form-input-wave project

--- Form-input-wave/script.js
+++ Form-input-wave/script.js
@@ -1,26 +1,34 @@
-const labels = document.querySelectorAll('.form-control label')  
- labels.forEach(label => {  
-   
-label.addEventListener('click',()=>{
-
-
-for (let i = 0; i < label.length; i++) {
-    
-    console.log(label);
-    
-}
+let inpu_email = document.getElementById("inpuEm");
+let lb_email = document.querySelectorAll("#lbemail > span");
+/////////
+let inpu_pass = document.getElementById("inpupas");
+let lb_pass = document.querySelectorAll("#password > span");
 
 
 
+inpu_email.addEventListener('focus', () => {
+  for (let i = 0; i < lb_email.length; i++) {
+    lb_email[i].classList.add("movimiento");
+  }
+});
 
+inpu_email.addEventListener('focusout', () => {
+  for (let i = 0; i < lb_email.length; i++) {
+    lb_email[i].classList.remove("movimiento");
+  }
+});
 
+//////////
 
-})
+inpu_pass.addEventListener('focus', () => {
+  for (let i = 0; i < lb_pass.length; i++) {
+    lb_pass[i].classList.add("movimiento");
+  }
+});
 
+inpu_pass.addEventListener('focusout', () => {
+  for (let i = 0; i < lb_pass.length; i++) {
+    lb_pass[i].classList.remove("movimiento");
+  }
+});
 
-
-
-
-
-
- })  
---
+++ sound-board/script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click',()=>{
+    let sond = document.getElementById("applause")
+    let audio = new Audio(sond);
+audio.play();
+
+
+
+})
